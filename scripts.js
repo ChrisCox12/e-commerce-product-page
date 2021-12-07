77a4ea6d899f99d639252e15367d33ca93e368a5
ChrisCox12 <chriscox9856@gmail.com>
2021-12-07 17:30:59 -0500

    added structure to html; added stylesheet; added some scripts

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const nav__sidebar = document.getElementById("nav__sidebar");
+const cart = document.getElementById("cart");
+
+function toggleSidebar() {
+    if(nav__sidebar.classList.contains("toggled--Sidebar")) {
+        nav__sidebar.classList.remove("toggled--Sidebar");
+    }
+    else {
+        nav__sidebar.classList.add("toggled--Sidebar");
+    }
+}
+
+function toggleCart() {
+    if(cart.classList.contains("toggled--Cart")) {
+        cart.classList.remove("toggled--Cart");
+    }
+    else {
+        cart.classList.add("toggled--Cart");
+    }
+}
